3cf8bfe420e93ad1e3ef038758637c652a482cdc
danielstallworthmit <danielstallworthmit@gmail.com>
2017-07-31 18:54:49 -0700

    Add search, update, and remove books functionality to maintain state on updates to books on search and main pages

--- src/App.js
+++ src/App.js
@@ -10,26 +10,69 @@ class BooksApp extends React.Component {
 
   componentDidMount() {
     BooksAPI.getAll().then(mainBooks => {
-      this.setState({mainBooks})
+      this.setState({mainBooks});
     })
   }
 
   bookSync = (searchBooks, mainBooks) => {
     return searchBooks.map(sb => {
-      sb.shelf = 'none'
+      sb.shelf = 'none';
       mainBooks.map(b => {
         if (sb.id === b.id) {
-          sb.shelf = b.shelf
+          sb.shelf = b.shelf;
         }
       })
-      return sb
+      return sb;
     })
+  }
+
+  searchBooks = (query, maxResults) => {
+    BooksAPI.search(query, maxResults).then((searchedBooks) => {
+      if (searchedBooks instanceof Array) {
+        const searchedBooksShelved = this.bookSync(searchedBooks, this.state.mainBooks);
+        this.setState({books: searchedBooksShelved});
+      } else {
+        this.setState({books: []});
+      }
+    })
+  }
+
+  // mapShelfFunc = (books, book, shelf)
+
+  updateBooks = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(mainBook => {
+      let stateCheck = 0;
+      let searchedBooks = []
+      const updatedMainBooks = this.state.mainBooks.map(b => {
+        if (b.id === mainBook.id) {
+          b.shelf = shelf;
+          stateCheck = 1;
+        }
+        return b;
+      })
+      if (stateCheck === 1) {
+        updatedMainBooks = updatedMainBooks.concat([book]);
+      }
+      if (this.state.books && this.state.books.length > 0) {
+        searchedBooks = this.state.books.map(b => {
+          if (b.id === mainBook.id) {
+            b.shelf = shelf;
+          }
+          return b;
+        })
+      }
+      this.setState({mainBooks: updatedMainBooks, books: searchedBooks});
+    })
+  }
+
+  removeSearchedBooks = () => {
+    this.setState({books: []})
   }
 
   render() {
     return (
       <div className="app">
-        
+
       </div>
     )
   }
